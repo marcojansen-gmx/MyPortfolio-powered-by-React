1e9812824a3c31d99f7bc32e0fdf423d85c29ec3
Marco <Marco.jansen@gmx.us>
2021-04-29 18:19:05 +0800

    removed old codebase folder and content

--- codebase/js/index.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    console.log("Welcome");
-    const params = new URLSearchParams(window.location.search);
-    if (params.has('logout')){
-      //Logout request, clean local session storage
-      delete sessionStorage.user;
-    }
-});
